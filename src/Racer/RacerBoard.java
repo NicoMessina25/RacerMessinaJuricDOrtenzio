cd86a6d33771d66919b6784999a905d7e4cd3a22
Nicolas Messina <nicolasmessina25@gmail.com>
2022-06-14 15:23:12 -0300

    Forgot to save xd

--- src/Racer/RacerBoard.java
+++ src/Racer/RacerBoard.java
@@ -1,6 +1,12 @@
 package Racer;
 
+import java.util.ArrayList;
+
 public class RacerBoard extends Board {
+	
+	private int amouQues = 50;
+	private ArrayList<Question> questions = new ArrayList<Question>(amouQues);
+	private ArrayList<Category> categories = new ArrayList<Caregory>();
 
 	public RacerBoard() {
 		
