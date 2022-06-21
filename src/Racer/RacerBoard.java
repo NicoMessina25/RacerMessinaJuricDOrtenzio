777e9e77bf35e7913f0c21821602b5da3ac29a8d
AlexeiJuric <alexei@ufasta.edu.ar>
2022-06-21 16:19:10 -0300

    arreglo de colores

--- src/Racer/RacerBoard.java
+++ src/Racer/RacerBoard.java
@@ -65,15 +65,15 @@ public class RacerBoard extends Board {
 		
 		
 		rb.getTeamColors().add(new TeamColor("Alpha Romeo", new Color(63, 5, 18))); //AlphaRomeo - 0
-		rb.getTeamColors().add(new TeamColor("Alpha Tauri", new Color((float) 32.5, (float) 38.4, (float) 47.5))); //AlphaTauri - 1
-		rb.getTeamColors().add(new TeamColor("Alpine", new Color((float) 94.9, (float) 58, (float) 77.6)));//Alpine - 2
-		rb.getTeamColors().add(new TeamColor("Aston Martin", new Color((float) 0, (float) 34.9, (float) 31)));//AstonMartin - 3
-		rb.getTeamColors().add(new TeamColor("Ferrari", new Color((float) 83.1, (float) 0, (float) 0)));//Ferrari - 4
-		rb.getTeamColors().add(new TeamColor("Haas", new Color((float) 100, (float) 100, (float) 100)));//Haas - 5
-		rb.getTeamColors().add(new TeamColor("McLaren", new Color((float) 95.7, (float) 52.2, (float) 0)));//McLaren - 6
-		rb.getTeamColors().add(new TeamColor("Mercedes", new Color((float) 0, (float) 0, (float) 0)));//Mercedes - 7
-		rb.getTeamColors().add(new TeamColor("Red Bull", new Color((float) 98.8, (float) 84.7, (float) 0)));//RedBull - 8
-		rb.getTeamColors().add(new TeamColor("Williams", new Color((float) 0, (float) 62.7, (float) 87.1)));//Williams - 9
+		rb.getTeamColors().add(new TeamColor("Alpha Tauri", new Color(32,38,47))); //AlphaTauri - 1
+		rb.getTeamColors().add(new TeamColor("Alpine", new Color(94,58,77)));//Alpine - 2
+		rb.getTeamColors().add(new TeamColor("Aston Martin", new Color(0, 34, 31)));//AstonMartin - 3
+		rb.getTeamColors().add(new TeamColor("Ferrari", new Color(83, 0,0)));//Ferrari - 4
+		rb.getTeamColors().add(new TeamColor("Haas", new Color(100, 100, 100)));//Haas - 5
+		rb.getTeamColors().add(new TeamColor("McLaren", new Color(95,52, 0)));//McLaren - 6
+		rb.getTeamColors().add(new TeamColor("Mercedes", new Color(0, 0,0)));//Mercedes - 7
+		rb.getTeamColors().add(new TeamColor("Red Bull", new Color(98,84,0)));//RedBull - 8
+		rb.getTeamColors().add(new TeamColor("Williams", new Color(0,62,87)));//Williams - 9
 		
 		do {
 			System.out.println("Choose the number of players (2-4)");
