2e4ee7abc1ac8744898df979e0e9e4fac3379315
Nicolas Messina <nicolasmessina25@gmail.com>
2022-06-28 14:17:58 -0300

    RacerGUI modificacion

--- src/Racer/RacerGUI.java
+++ src/Racer/RacerGUI.java
@@ -4,6 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.util.Scanner;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -33,11 +36,33 @@ public class RacerGUI extends JFrame {
 	 */
 	public RacerGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 597, 560);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		
+		String path = "C:\\Users\\USUARIO\\Desktop\\NICO\\UniversidadNico\\Año 2\\Programación B\\eclipse-workspace\\RacerMessinaJuricDOrtenzio\\imagenes";
+		File folder = new File(path);
+		File[] listOfFiles = folder.listFiles();
+		
+		
+		String imgFileName;
+		try {
+			imgFileName = listOfFiles[0].getCanonicalPath();
+			JButton b = new JButton(new ImageIcon(imgFileName));
+			
+			contentPane.add(b, BorderLayout.NORTH);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+
+		
 	}
 
 }
